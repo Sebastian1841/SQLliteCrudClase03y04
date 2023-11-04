e813a769f45bc7df7332af54105850b7cb603daa
Sebastian1841 <149020094+Sebastian1841@users.noreply.github.com>
2023-11-04 05:40:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sqllitecrud/Contacto.java
@@ -0,0 +1,81 @@
+package com.example.sqllitecrud;
+
+public class Contacto {
+
+    int id;
+    String nombre;
+    String apellido;
+    String email;
+    String telefono;
+    String ciudad;
+
+
+    public  Contacto(){
+
+    }
+
+    public Contacto(String nombre, String apellido, String email, String telefono, String ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+    }
+
+    public Contacto(int id, String nombre, String apellido, String email, String telefono, String ciudad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
